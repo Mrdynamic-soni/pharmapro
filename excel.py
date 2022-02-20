6d99deecd41e2a95e83e0c8308efd499db6a2a91
Mrdynamic-soni <ssoni5930@gmail.com>
2022-02-20 09:58:53 +0530

    main file corrected

--- excel.py
+++ excel.py
@@ -6,8 +6,10 @@ path = "C:\\Users\\Acer\\Desktop\\PHARMAPRO\\Data.xlsx"
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
 
-# cell = sheet_obj.cell(row=1,column = 1)
-# print(cell.value)
-
+# method to print all the colmns from excel 
 for i in range(1,sheet_obj.max_column+1):
     print(sheet_obj.cell(row=1,column=i).value)
+
+# append data on the file
+sheet_obj.cell(row=2,column=2).value = "soni"
+wb_obj.save(path)
